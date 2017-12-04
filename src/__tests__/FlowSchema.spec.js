3a37d1932dd2af85b35c40a71c508806380f0c4b
Matt Brennan <matt@153.io>
2017-12-04 11:16:22 +0000

    update tests for date type

--- src/__tests__/FlowSchema.spec.js
+++ src/__tests__/FlowSchema.spec.js
@@ -140,6 +140,15 @@ test('should convert simple types', (t) => {
 
   t.deepEqual(
     convertSchema({
+      id: 'Date',
+      type: 'date',
+    }),
+    flow('Date')
+      .id('Date'),
+  );
+
+  t.deepEqual(
+    convertSchema({
       id: 'Null',
       type: 'null',
     }),
