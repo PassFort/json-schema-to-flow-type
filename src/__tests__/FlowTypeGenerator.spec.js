cf338202428b4fdebe910be746db936ce0b2f038
unconfident <unconfidentguy@yandex.com>
2018-03-27 06:41:53 +0200

    Fix code style of function declarations in FlowTypeGenerator.spec.js

--- src/__tests__/FlowTypeGenerator.spec.js
+++ src/__tests__/FlowTypeGenerator.spec.js
@@ -15,28 +15,22 @@ import {
   toFlowType, upperCamelCase,
 } from '../FlowTypeGenerator';
 
-function flowSchemaFromObject(obj: Object): FlowSchema {
+const flowSchemaFromObject = (obj: Object): FlowSchema =>
   // $FlowIgnore: this code is correct, but flow complains because of how _.cloneDeepWith is documented
-  return _.cloneDeepWith(obj, (item) => {
-    if (_.isPlainObject(item)) {
-      return new FlowSchema(item);
-    }
+  _.cloneDeepWith(obj, (item) => {
+    if (_.isPlainObject(item)) return new FlowSchema(item);
   });
-}
 
-function codeFromFlowSchema(flowSchemaLike) {
-  const schema = flowSchemaFromObject(flowSchemaLike);
+const codeFromFlowSchema = (flowSchemaLike) =>
+  generate(
+    t.typeAlias(
+      t.identifier(upperCamelCase(flowSchemaLike.$id)),
+      null,
+      toFlowType(flowSchemaFromObject(flowSchemaLike)),
+    )
+  ).code;
 
-  const ast = t.typeAlias(
-    t.identifier(upperCamelCase(schema.$id)),
-    null,
-    toFlowType(schema),
-  );
-
-  return generate(ast).code;
-}
-
-function parseCode(code) {
+const parseCode = (code) => {
   try {
     return babylon.parse(code, {
       sourceType: 'module',
@@ -50,13 +44,14 @@ function parseCode(code) {
     }
 
     const posData = err.message.match(/ \((\d+):(\d+)\)$/);
+
     if (posData !== null) {
       err.message += `\n${codeFrame(code, ...posData.slice(1, 3))}`;
     }
 
     throw err;
   }
-}
+};
 
 test('produces syntactically valid output', (t) => {
   const code = codeFromFlowSchema({
